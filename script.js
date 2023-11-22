39e247e54369eb14de16cd77107956f750ddda76
MGChaitra <mgchaitra24@gmail.com>
2023-11-22 15:23:08 +0530

    mainnew

---
+++ script.js
@@ -0,0 +1,62 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x-circle');
+    navbar.classList.toggle('active');
+});
+
+let selections=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    selections.forEach(sec => {
+        let top=window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    menuIcon.classList.remove('bx-x-circle');
+    navbar.classList.remove('active');
+
+};
+
+
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .Certificates-container, .projects-box, .contact form' , { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img' , { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content' , { origin: 'right'});
+
+
+
+const typed =new Typed('.multiple-text', {
+    strings: ['Web Development', 'Python', 'C', 'Java', 'App Development', 'Machine Learning', 'MySQL'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+
+});
